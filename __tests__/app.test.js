871339dd8277d72f47b053a297cc696ebed8939e
LENA EIVY <applena@gmail.com>
2018-12-28 18:42:17 -0800

    passing linter

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,33 +28,33 @@ describe('reads a file and changes it to upper case then writes it to a new file
   it('reads a file', () => {
     listener.success(__dirname+'/test.file.txt');
     expect(successSpy).resolves;
-  })
+  });
 
   it('calls the saved function when it successfully saves the file', () => {
     listener.saved(__dirname+'/test.file.txt');
     expect(savedSpy).toBeCalled();
-  })
+  });
 
   it('calls the success function when it successfully reads the file', () => {
     listener.success(__dirname+'/test.file.txt');
     expect(successSpy).toBeCalled();
-  })
+  });
 
   it('calls the errorHandler function when there is an error', () => {
     listener.errorHandler(__dirname+'/test.file.txt');
     expect(errorHandlerSpy).toBeCalled();
-  })
+  });
 
   it('console logs success when a file is saved', () => {
     listener.success(__dirname+'/test.file.txt');
-    expect(consoleSpy).toHaveBeenCalledWith("successfully read");
-  })
+    expect(consoleSpy).toHaveBeenCalledWith('successfully read');
+  });
 
   it('requires a file name to read', () => {
     expect(()=>{
       file.alterFile();
     }).toThrow('you must enter a file path');
-  })
+  });
 
   it('can uppercase a buffer', () => {
     const str = 'test words';
@@ -62,7 +62,7 @@ describe('reads a file and changes it to upper case then writes it to a new file
     const b = Buffer.from(str);
     const B = Buffer.from(STR);
     expect(file.convertBuffer(b)).toEqual(B);
-  })
+  });
 
   afterAll(() => {
     errorHandlerSpy.mockRestore();
@@ -72,4 +72,4 @@ describe('reads a file and changes it to upper case then writes it to a new file
     consoleSpy.mockRestore();
   });
 
-})
+});
